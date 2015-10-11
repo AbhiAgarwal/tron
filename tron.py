5abe80bb0dc7f178956ebb51d83b701de3da1cae
Abhi Agarwal <me@abhiagarwal.com>
2015-10-10 23:00:30 -0400

    Cleaning up file structure

--- tron.py
+++ tron.py
@@ -6,7 +6,7 @@ screen = pygame.display.set_mode((1000,1003))
 class LightBike(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('light3.gif')
+        self.image = pygame.image.load('./images/light3.gif')
         self.rect = self.image.get_rect()
         self.prev_pos = []
         self.speed = 12
@@ -21,19 +21,19 @@ class LightBike(pygame.sprite.Sprite):
             self.prev_pos.pop(0)
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
                 self.moveright()
-                self.image = pygame.image.load('light3.gif')
+                self.image = pygame.image.load('./images/light3.gif')
         elif pygame.key.get_pressed()[pygame.K_DOWN]:
                 self.movedown()
                 
-                self.image = pygame.image.load('light4.gif')
+                self.image = pygame.image.load('./images/light4.gif')
         elif pygame.key.get_pressed()[pygame.K_LEFT]:
                 self.moveleft()
                 
-                self.image = pygame.image.load('light2.gif')
+                self.image = pygame.image.load('./images/light2.gif')
         elif pygame.key.get_pressed()[pygame.K_UP]:
                 self.moveup()
                 
-                self.image = pygame.image.load('light1.gif')
+                self.image = pygame.image.load('./images/light1.gif')
     def movedown(self):
         self.rect.centery += self.speed
     def moveup(self):
@@ -46,7 +46,7 @@ class LightBike(pygame.sprite.Sprite):
 def main():
     lb = LightBike()
     Sprites = pygame.sprite.Group(lb)
-    pygame.mixer.music.load('music.mp3')
+    pygame.mixer.music.load('./music/music.mp3')
     pygame.mixer.music.play(-1)
     clock = pygame.time.Clock()
     while True:
@@ -63,7 +63,7 @@ def main():
             lb.rect.centery = 0
         if lb.rect.centerx <= 0:
             lb.rect.centerx = 0
-        bgi = pygame.image.load('BlackBackground.gif')
+        bgi = pygame.image.load('./images/bg.gif')
         screen.blit(bgi,(0,0))
         Sprites.update()
         Sprites.draw(screen)
